ae1ba0510302077b65b9d504e8f321d6d827ecf0
knsugiyama <knsugiyama15@gmail.com>
2020-05-16 08:33:47 +0900

    Update env.fish

--- .files/.config/fish/env.fish
+++ .files/.config/fish/env.fish
@@ -1,9 +1,1 @@
-set -x PATH $HOME/.anyenv/bin $PATH
-eval (anyenv init - | source)
-set -x PATH "/usr/local/opt/openjdk/bin" $PATH
-set -x PATH "/usr/local/opt/icu4c/bin" $PATH
-set -x PATH "/usr/local/opt/icu4c/sbin" $PATH
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-set -x PATH $HOME/.jenv/bin $PATH
-status --is-interactive; and source (jenv init -|psub)
-set -x JAVA_HOME (jenv prefix)
+
