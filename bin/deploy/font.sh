9b54d7c144eff5b7093840c4946a00e5eae41ae7
knsugiyama <knsugiyama15@gmail.com>
2020-05-16 11:27:50 +0900

    Update font.sh

--- bin/deploy/font.sh
+++ bin/deploy/font.sh
@@ -7,15 +7,15 @@ set -ue
 OS=$(os_detect)
 
 if [ ${OS} == 'osx' ]; then
-    # brew cask install font-cica
+    brew cask install font-cica
     brew cask install font-hackgen
 
 elif [ ${OS} == 'linux' ]; then
     # cica
-    # sudo mkdir -p /usr/share/fonts/cica
-    # CICA_RELEASES_URL="https://api.github.com/repos/miiton/Cica/releases"
-    # sudo curl -sfL "$CICA_RELEASES_URL" | jq -r '.[0].assets | .[].browser_download_url' | grep with_emoji.zip | xargs -I{} curl -fL -o /tmp/Cica.zip "{}"
-    # (cd /tmp && sudo unzip -o Cica.zip -d /usr/share/fonts/cica)
+    sudo mkdir -p /usr/share/fonts/cica
+    CICA_RELEASES_URL="https://api.github.com/repos/miiton/Cica/releases/latest"
+    sudo curl -sfL "$CICA_RELEASES_URL" | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \" | xargs -I{} curl -fL -o /tmp/Cica.zip "{}"
+    (cd /tmp && sudo unzip -o Cica.zip -d /usr/share/fonts/cica)
 
     # HackGen
     sudo mkdir -p /usr/share/fonts/HackGen
