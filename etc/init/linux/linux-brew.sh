ae41ad512fb215c4ebd28fd871060a93be0becd5
knsugiyama <knsugiyama15@gmail.com>
2020-07-22 00:38:38 +0900

    install 修正

--- etc/init/linux/linux-brew.sh
+++ etc/init/linux/linux-brew.sh
@@ -3,15 +3,8 @@
 set -ue
 
 # linuxbrew の導入
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-# test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-# test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-# test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-# echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-
-git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-mkdir /home/linuxbrew/.linuxbrew/bin
-ln -s ~/.linuxbrew/Homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
