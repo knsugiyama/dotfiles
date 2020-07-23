34e0430e4c67dd38b2e661a24b66608c318f873f
knsugiyama <knsugiyama15@gmail.com>
2020-07-23 14:24:38 +0900

    anyenv周りを修正

--- etc/init/linux/git-clones/anyenv.sh
+++ etc/init/linux/git-clones/anyenv.sh
@@ -5,3 +5,5 @@ git clone https://github.com/anyenv/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >>~/.bash_profile
 # for fish
 echo 'set -x PATH $HOME/.anyenv/bin $PATH' >>~/.config/fish/env.fish
+
+. ~/.bash_profile
