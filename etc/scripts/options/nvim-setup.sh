36a874034537de2af6c9aedbbce0c6c63929164d
knsugiyama <knsugiyama15@gmail.com>
2020-09-15 20:29:05 +0900

    neovimインストール方法変更

--- etc/scripts/options/nvim-setup.sh
+++ etc/scripts/options/nvim-setup.sh
@@ -10,18 +10,20 @@ echo "set -x XDG_CONFIG_HOME $HOME/.config" >> ~/.config/fish/env.fish
 echo "set -x XDG_CACHE_HOME $HOME/.cache" >> ~/.config/fish/env.fish
 
 if [ ${OS} == 'osx' ]; then
-    brew install neovim --HEAD
+    # brew install neovim --HEAD
+    brew install neovim
 elif [ ${OS} == 'linux' ]; then
-    sudo add-apt-repository ppa:neovim-ppa/unstable
-    sudo apt update
+    # sudo add-apt-repository ppa:neovim-ppa/unstable
+    # sudo apt update
     sudo apt install -y neovim
 fi
 
 # python
 pyenv virtualenv 3.8.5 neovim3
-pyenv virtualenv 2.7.18 neovim2
 pyenv shell neovim3
 pip install pynvim
+
+pyenv virtualenv 2.7.18 neovim2
 pyenv shell neovim2
 pip install pynvim
 
