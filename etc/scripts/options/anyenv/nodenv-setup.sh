ef199c3feeb62e323cf6e835a129c85ccd8283f4
knsugiyama <knsugiyama15@gmail.com>
2020-09-19 01:17:46 +0900

    Update nodenv-setup.sh

--- etc/scripts/options/anyenv/nodenv-setup.sh
+++ etc/scripts/options/anyenv/nodenv-setup.sh
@@ -4,5 +4,5 @@ touch $(nodenv root)/default-packages
 mkdir -p $(nodenv root)/plugins
 git clone https://github.com/pine/nodenv-yarn-install.git $(nodenv root)/plugins/nodenv-yarn-install
 
-nodenv install 14.5.0
-nodenv global 14.5.0
+nodenv install 14.11.0
+nodenv global 14.11.0
