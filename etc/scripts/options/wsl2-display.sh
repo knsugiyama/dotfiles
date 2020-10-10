769e74b1dc9def1c4a6bc843a7dea9f1e03f6323
knsugiyama <knsugiyama15@gmail.com>
2020-10-10 14:35:42 +0900

    Create wsl2-display.sh

---
+++ etc/scripts/options/wsl2-display.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# xserver tools
+sudo apt install -y xauth x11-apps
+
+{
+    echo 'umask 022'
+    echo 'set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '\''{print $2}'\''):0'
+    echo 'set -x TZ "Asia/Tokyo"'
+    echo 'set -x LANG "ja_JP.UTF-8"'
+    echo 'set -x LC_ALL "ja_JP.UTF-8"'
+} >>~/.config/fish/env.fish
