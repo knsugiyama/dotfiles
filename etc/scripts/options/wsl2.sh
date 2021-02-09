75daf70136f970e5257bd39b44286e08d2d3e7ec
knsugiyama <knsugiyama15@gmail.com>
2021-02-10 00:59:03 +0900

    wsl2の設定を変更

--- etc/scripts/options/wsl2.sh
+++ etc/scripts/options/wsl2.sh
@@ -3,11 +3,5 @@
 sudo ln -sfnv ${DOTPATH}/wsl.conf /etc/wsl.conf
 
 # credential
-sudo touch /usr/bin/git-credential-manager
-sudo sh -c "echo \#\!/bin/sh >>/usr/bin/git-credential-manager"
-sudo sh -c "echo exec /mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe $@ >>/usr/bin/git-credential-manager"
-
-sudo chmod +x /usr/bin/git-credential-manager
-
 sed '/  helper/d' ~/.dotfiles/.gitconfig.credential -i
-echo '  helper = manager' >>~/.dotfiles/.gitconfig.credential
+echo '  helper =  /mnt/c/Program\\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe' >>~/.dotfiles/.gitconfig.credential
