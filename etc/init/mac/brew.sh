614ed1b98a35a8cde423bbe50ba78c522e2ba26f
knsugiyama <knsugiyama15@gmail.com>
2021-03-01 09:56:40 +0900

    Update brew.sh

--- etc/init/mac/brew.sh
+++ etc/init/mac/brew.sh
@@ -5,5 +5,8 @@
 echo $(log_echo "install homebrew")
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/user/.zprofile
+
 echo $(log_echo "update homebrew")
 brew update --verbose
+brew bundle
