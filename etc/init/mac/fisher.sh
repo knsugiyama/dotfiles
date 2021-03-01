9b5a1aa2486d6503027357778d865cdfbac30e23
knsugiyama <knsugiyama15@gmail.com>
2021-03-01 10:22:05 +0900

    Update fisher.sh

--- etc/init/mac/fisher.sh
+++ etc/init/mac/fisher.sh
@@ -5,4 +5,4 @@ if [ ! -e ~/.config/fish/functions/fisher.fish ]; then
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 fi
 
-fish -c "fisher"
+fish -c "fisher update"
