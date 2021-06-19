22fc42d1c6e57a890f4371ab9ff23ac4f99aa1e1
knsugiyama15 <knsugiyama15@gmail.com>
2021-06-19 23:09:30 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,6 @@ set -g fish_prompt_pwd_dir_length 1
 set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
-set -g theme_color_scheme dracula
-set -g theme_display_date no
-set -g theme_display_cmd_duration no
 
 eval (hub alias -s)
 direnv hook fish | source
