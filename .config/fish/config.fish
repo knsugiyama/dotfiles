ebe65577ce2c386fd08b7866cfcaeee052fd9c19
knsugiyama15 <knsugiyama15@gmail.com>
2022-05-13 17:26:25 +0900

    docker 追加

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-. ~/.config/fish/alias.fish
-. ~/.config/fish/myfunctions.fish
-. ~/.config/fish/env.fish
-
-set -x LANG en_US.UTF-8
-set -U EDITOR vim
-
-eval (hub alias -s)
-direnv hook fish | source
