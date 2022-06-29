fd85cd92dc718d53918658638ba7cd7fd5e000de
knsugiyama15 <knsugiyama15@gmail.com>
2022-06-30 00:50:12 +0900

    add multipass

---
+++ misc/cloud-init/cloud-config-btp.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# 事前に作成した.pubファイルを変数に読み込む
+# 作成してない場合 : ssh-keygen -t rsa -b 4096 -C "your_email@example.com" -f $HOME/.ssh/multipass_docker
+$AUTHORIZED_KEYS = Get-Content $HOME/.ssh/multipass_docker.pub
+
+cat << EOF > ~/multipass_btp.yaml
+---
+#cloud config
+
+locale: en_US.UTF8
+timezone: Asia/Tokyo
+package_upgrade: true
+users:
+  - name: ubuntu
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    ssh-authorized-keys:
+      - ${AUTHORIZED_KEYS}
+packages:
+  - docker
+  - avahi-daemon
+  - apt-transport-https
+  - ca-certificates
+  - curl
+  - gnupg
+  - lsb-release
+  - build-essential
+  - unzip
+runcmd:
+  # ユーザー作成
+  - sudo curl -fsSL https://get.docker.com | sudo bash
+  - sudo systemctl enable docker
+  - sudo systemctl enable -s HUP ssh
+  - sudo groupadd docker
+  - sudo usermod -aG docker ubuntu
+  # CF Cli 追加
+  - wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add
+  - echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+  - sudo apt-get update
+  - sudo apt-get install cf8-cli
+  # AWS Cli 追加
+  - curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  - unzip awscliv2.zip
+  - sudo ./aws/install
+
+EOF
---
+++ misc/cloud-init/memo.md
@@ -0,0 +1,17 @@
+## ~/.ssh/config への記載例
+Host docker.local
+  Hostname docker.local
+  IdentityFile ~/.ssh/multipass_docker
+  User ubuntu
+  Port 22
+
+## 起動方法
+multipass launch --name docker-vm --cpus 4 --mem 8G --disk 20G --cloud-init multipass_docker.yaml 20.04
+
+## ディレクトリ マウント
+multipass mount ${HOME} docker:${HOME}
+とか
+multipass mount ./src docker:/home/ubuntu/src
+
+## ディレクトリ アンマウント
+multipass unmount docker:${HOME}
