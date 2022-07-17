a9379e908bbcbbf56dc569ac4577c9d0eacc0b20
knsugiyama15 <knsugiyama15@gmail.com>
2022-07-18 07:20:38 +0900

    update

--- dist/Ubuntu/setup.sh
+++ dist/Ubuntu/setup.sh
@@ -4,5 +4,3 @@ set -eu
 
 # make ファイルのシンボリックリンクをrootに移動
 ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/Makefile ${HOME}/.dotfiles/Makefile
-# .zshenv ファイル のシンボリックリンクをrootに移動
-ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/.zshenv ${HOME}/.dotfiles/.zshenv
