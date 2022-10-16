a519b5ce47da1faa05854f46df08517f9ac8b24c
knsugiyama15 <knsugiyama15@gmail.com>
2022-10-16 10:51:59 +0900

    altキーでGoogle日本語入力を切り替えられるように変更

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,24 +1,24 @@
-local simpleCmd = false
+local simpleAlt = false
 local map = hs.keycodes.map
 
--- 左右コマンドキーで英語、Google日本語入力を切り替える
+-- 左右Altキーで英語、Google日本語入力を切り替える
 local function eikanaEvent(event)
   local c = event:getKeyCode()
   local f = event:getFlags()
   if event:getType() == hs.eventtap.event.types.keyDown then
-    if f['cmd'] then
-      simpleCmd = true
+    if f['alt'] then
+      simpleAlt = true
     end
   elseif event:getType() == hs.eventtap.event.types.flagsChanged then
-    if not f['cmd'] then
-      if simpleCmd == false then
-        if c == map['cmd'] then
+    if not f['alt'] then
+      if simpleAlt == false then
+        if c == map['alt'] then
           hs.keycodes.setMethod('Alphanumeric (Google)')
-        elseif c == map['rightcmd'] then
+        elseif c == map['rightalt'] then
           hs.keycodes.setMethod('Hiragana (Google)')
         end
       end
-      simpleCmd = false
+      simpleAlt = false
     end
   end
 end
