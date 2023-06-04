1e7b1f696d9efbcc57da396170019ad0be016cc7
knsugiyama15 <knsugiyama15@gmail.com>
2023-06-04 22:09:52 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,23 +1,4 @@
 -- https://wezfurlong.org/wezterm/config/files.html
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
 
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
--- Changing the color scheme and font:
-config.color_scheme = 'Dracula (Official)'
-
-config.font = wezterm.font 'HackGen35 Console NF'
-config.font =
-  wezterm.font('JetBrains Mono')
-
--- and finally, return the configuration to wezterm
-return config
+require 'default_settings'
+-- require 'launch_menu'
