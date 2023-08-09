31e3969655540cf5821f839c529b59cc12ac0a31
knsugiyama15 <knsugiyama15@gmail.com>
2023-08-09 10:04:24 +0900

    update

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,15 +1,31 @@
-local simpleAlt = false
+local log = hs.logger.new('mymodule','debug')
 local map = hs.keycodes.map
+local keyDown = hs.eventtap.event.types.keyDown
+local keyUp = hs.eventtap.event.types.keyUp
+local flagsChanged = hs.eventtap.event.types.flagsChanged
+local eventtap = hs.eventtap
+local bit = require("libs/bit")
 
+--[[
+-- auto reload
+--]]
+autoReload = hs.pathwatcher.new(os.getenv('HOME') .. '/.config/hammerspoon/init.lua', function()
+  hs.timer.doAfter(0.1, hs.reload)
+end):start()
+
+--[[
 -- 左右Altキーで英語、Google日本語入力を切り替える
+--]]
+local simpleAlt = false
+
 local function eikanaEvent(event)
   local c = event:getKeyCode()
   local f = event:getFlags()
-  if event:getType() == hs.eventtap.event.types.keyDown then
+  if event:getType() == keyDown then
     if f['alt'] then
       simpleAlt = true
     end
-  elseif event:getType() == hs.eventtap.event.types.flagsChanged then
+  elseif event:getType() == flagsChanged then
     if not f['alt'] then
       if simpleAlt == false then
         if c == map['alt'] then
@@ -23,6 +39,33 @@ local function eikanaEvent(event)
   end
 end
 
-eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
+eikana = eventtap.new({keyDown, flagsChanged}, eikanaEvent)
 eikana:start()
 
+--[[
+-- esc キー押下でIME切り替えをする
+--]]
+switchToEisuOnEscape = eventtap.new({keyDown}, function(e)
+  if hs.keycodes.map[e:getKeyCode()] == 'escape' then
+    hs.keycodes.setMethod('Alphanumeric (Google)')
+  end
+end):start()
+
+--[[
+-- ControlとCommandキーの入れ替えをインターセプトで実現する
+--]]
+swapCtrlAndCmd = eventtap.new({keyDown, keyUp}, function(e)
+    -- Controlキーの場合、Commandキーに変換
+    local flags = e:getFlags()
+    -- log.i(flags)
+    if flags.ctrl and not flags.cmd then
+        flags.ctrl = false
+        flags.cmd = true
+        e:setFlags(flags)
+    -- Commandキーの場合、Controlキーに変換
+    elseif flags.cmd and not flags.ctrl then
+        flags.cmd = false
+        flags.ctrl = true
+        e:setFlags(flags)
+    end
+end):start()
