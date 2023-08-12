9d27351f42f28d221633408a678e156e4c281be9
knsugiyama15 <knsugiyama15@gmail.com>
2023-08-12 10:36:08 +0900

    update

--- .config/nvim/lua/plugin_configs/neo-tree.lua
+++ .config/nvim/lua/plugin_configs/neo-tree.lua
@@ -1,210 +1,22 @@
 require("neo-tree").setup({
+  popup_border_style = "rounded",
   filesystem = {
     filtered_items = {
       hide_dotfiles = false,
     },
     follow_current_file = { enabled = true },
   },
+  window = {
+    mappings = {
+      ["l"] = "open",
+      ["h"] = "close_node",
+    }
+  },
+  git_status = {
+    window = {
+      position = "float",
+    },
+  },
 })
-vim.keymap.set("n", "gx", "<Cmd>Neotree toggle<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "G,", "<Cmd>Neotree git_status<CR>", { noremap = true, silent = true })
--- require("neo-tree").setup({
---   close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
---   popup_border_style = "rounded",
---   enable_git_status = true,
---   enable_diagnostics = true,
---   open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
---   default_component_configs = {
---     container = {
---         enable_character_fade = true
---     },
---     indent = {
---         indent_size = 2,
---         padding = 1, -- extra padding on left hand side
---         with_markers = true,
---         indent_marker = "│",
---         last_indent_marker = "└",
---         highlight = "NeoTreeIndentMarker",
---         with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
---         expander_collapsed = "",
---         expander_expanded = "",
---         expander_highlight = "NeoTreeExpander",
---     },
---     icon = {
---       folder_closed = "",
---       folder_open = "",
---       folder_empty = "ﰊ",
---       default = "*",
---       highlight = "NeoTreeFileIcon"
---     },
---     modified = {
---         symbol = "[+]",
---         highlight = "NeoTreeModified",
---     },
---     name = {
---       trailing_slash = false,
---       use_git_status_colors = true,
---       highlight = "NeoTreeFileName",
---     },
---     git_status = {
---       highlight = "NeoTreeDimText", -- if you remove this the status will be colorful
---     },
---   },
---   event_handlers = {
---     {
---       event = "file_opened",
---       -- selene: allow(unused_variable)
---       ---@diagnostic disable-next-line: unused-function, unused-local
---       handler = function(file_path)
---         --auto close
---         require("neo-tree.command").execute({ action = "close" })
---       end,
---     },
---     {
---       event = "file_added",
---       handler = function(file_path)
---         require("neo-tree.utils").open_file({}, file_path)
---       end,
---     },
---   },
---   window = {
---     position = "left",
---     width = 40,
---     mapping_options = {
---       noremap = true,
---       nowait = true,
---     },
---     mappings = {
---       ["<2-LeftMouse>"] = "open",
---       ["<cr>"] = "open",
---       ["<esc>"] = "cancel", -- close preview or floating neo-tree window
---       ["P"] = { "toggle_preview", config = { use_float = true } },
---       ["l"] = "focus_preview",
---       ["S"] = "open_split",
---       ["s"] = "open_vsplit",
---       ["t"] = "open_tabnew",
---       ["w"] = "open_with_window_picker",
---       ["C"] = "close_node",
---       ["z"] = "close_all_nodes",
---       ["a"] = {
---         "add",
---         config = {
---           show_path = "none" -- "none", "relative", "absolute"
---         }
---       },
---       ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
---       ["d"] = "delete",
---       ["r"] = "rename",
---       ["y"] = "copy_to_clipboard",
---       ["x"] = "cut_to_clipboard",
---       ["p"] = "paste_from_clipboard",
---       ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
---       ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
---       ["q"] = "close_window",
---       ["R"] = "refresh",
---       ["<"] = "prev_source",
---       [">"] = "next_source",
---       ["?"] = "show_help",
---     }
---   },
---   filesystem = {
---     filtered_items = {
---       visible = false, -- when true, they will just be displayed differently than normal items
---       hide_dotfiles = false,
---       hide_gitignored = true,
---       hide_by_name = {
---         ".DS_Store",
---         "thumbs.db",
---       },
---       never_show = { -- remains hidden even if visible is toggled to true
---       },
---     },
---     follow_current_file = {
---         enabled = false, -- This will find and focus the file in the active buffer every
---         leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
---     },
---     -- time the current file is changed while the tree is open.
---     use_libuv_file_watcher = false, -- This will use the OS level file watchers
---     -- to detect changes instead of relying on nvim autocmd events.
---     hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
---     -- in whatever position is specified in window.position
---     -- "open_split",  -- netrw disabled, opening a directory opens within the
---     -- window like netrw would, regardless of window.position
---     -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
---     window = {
---       mappings = {
---           ["<bs>"] = "navigate_up",
---           ["."] = "set_root",
---           ["H"] = "toggle_hidden",
---           ["/"] = "fuzzy_finder",
---           ["D"] = "fuzzy_finder_directory",
---           ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
---           ["f"] = "filter_on_submit",
---           ["<c-x>"] = "clear_filter",
---           ["[g"] = "prev_git_modified",
---           ["]g"] = "next_git_modified",
---       },
---       fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
---           ["<down>"] = "move_cursor_down",
---           ["<C-n>"] = "move_cursor_down",
---           ["<up>"] = "move_cursor_up",
---           ["<C-p>"] = "move_cursor_up",
---       },
---     },
---   },
---   buffers = {
---     follow_current_file = {
---         enabled = true, -- This will find and focus the file in the active buffer every time
---         --              -- the current file is changed while the tree is open.
---         leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
---     },
---     group_empty_dirs = true, -- when true, empty folders will be grouped together
---     show_unloaded = true,
---     window = {
---       mappings = {
---         ["bd"] = "buffer_delete",
---         ["<bs>"] = "navigate_up",
---         ["."] = "set_root",
---       },
---     },
---   },
---   git_status = {
---     window = {
---       position = "float",
---       mappings = {
---         ["A"] = "git_add_all",
---         ["gu"] = "git_unstage_file",
---         ["ga"] = "git_add_file",
---         ["gr"] = "git_revert_file",
---         ["gc"] = "git_commit",
---         ["gp"] = "git_push",
---         ["gg"] = "git_commit_and_push",
---       },
---     },
---   },
---   renderers = {
---     directory = {
---       { "indent" },
---       { "icon" },
---       { "current_filter" },
---       { "name" },
---       { "clipboard" },
---       { "diagnostics", errors_only = true },
---     },
---     file = {
---       { "indent" },
---       { "icon" },
---       {
---         "name",
---         use_git_status_colors = true,
---         zindex = 10,
---       },
---       { "clipboard" },
---       { "bufnr" },
---       { "modified" },
---       { "diagnostics" },
---       { "git_status" },
---     },
---   },
--- })
--- vim.keymap.set("n", "ts", ":Neotree reveal=false<CR>", { noremap = true, silent = true, desc = '[T]oggle [S]ide menu'})
+vim.keymap.set("n", "gx", "<Cmd>Neotree toggle<CR>", { noremap = true, silent = true, desc='Neotree toggle'  })
+vim.keymap.set("n", "G,", "<Cmd>Neotree git_status<CR>", { noremap = true, silent = true, desc='Git Status' })
