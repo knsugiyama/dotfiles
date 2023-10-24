fe0d4d47f2f096d18ff9baf91371b1dedcac4f74
knsugiyama <knsugiyama15@gmail.com>
2023-10-24 22:40:34 +0900

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,6 +4,14 @@ Write-Host "#####"
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
+# Write-Host "#####"
+# Write-Host "最新のwingetをインストール"
+# Write-Host "#####"
+
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.msixbundle -UseBasicParsing
+Add-AppxPackage -Path winget.msixbundle
+Remove-Item winget.msixbundle
+
 Write-Host "#####"
 Write-Host "git clone"
 Write-Host "#####"
