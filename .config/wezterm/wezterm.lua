2a9825608af89cee2c92ab2cabe060fd6ba5725c
knsugiyama15 <knsugiyama15@gmail.com>
2024-04-10 18:58:58 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 -- https://wezfurlong.org/wezterm/config/files.html
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -13,63 +13,63 @@ end
 
 config.font_size = 16.0
 
-config.font = wezterm.font_with_fallback {
-  {family = 'Moralerspace Argon NF'},
-  {family = 'HackGen35 Console NF'}
-}
+config.font = wezterm.font_with_fallback({
+  { family = "Moralerspace Argon NF" },
+  { family = "HackGen35 Console NF" },
+})
 
 config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
 
 -- change default shell
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  config.default_prog = { 'pwsh.exe', '-l' }
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.default_prog = { "pwsh.exe", "-l" }
   config.font_size = 12.0
 end
 
 -- This is where you actually apply your config choices
 -- Changing the color scheme and font:
-config.color_scheme = 'MaterialDesignColors'
+config.color_scheme = "MaterialDesignColors"
 
 config.initial_rows = 40
 config.initial_cols = 150
-config.default_cursor_style = 'BlinkingUnderline'
+config.default_cursor_style = "BlinkingUnderline"
 
 config.leader = { key = "Space", mods = "CTRL|SHIFT" }
 
 -- 背景透過
 config.window_background_opacity = 0.90
 
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 
 -- ショートカットキー設定
 local act = wezterm.action
 config.keys = {
   -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
   {
-    key = 'f',
-    mods = 'SHIFT|META',
+    key = "f",
+    mods = "SHIFT|META",
     action = act.ToggleFullScreen,
   },
   -- Ctrl+Shift+tで新しいタブを作成
   {
-    key = 't',
-    mods = 'SHIFT|CTRL',
-    action = act.SpawnTab 'CurrentPaneDomain',
+    key = "t",
+    mods = "SHIFT|CTRL",
+    action = act.SpawnTab("CurrentPaneDomain"),
   },
   -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
   {
-    key = 'd',
-    mods = 'SHIFT|CTRL',
-    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    key = "d",
+    mods = "SHIFT|CTRL",
+    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
   },
   -- Ctrl+Backspaceで前の単語を削除
   {
     key = "Backspace",
     mods = "CTRL",
-    action = act.SendKey {
+    action = act.SendKey({
       key = "w",
       mods = "CTRL",
-    },
+    }),
   },
 }
 
