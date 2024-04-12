344b27461f2981770cf7988d7e7707d034bb0f62
knsugiyama15 <knsugiyama15@gmail.com>
2024-04-12 19:06:27 +0900

    update

--- tools/asdf/install.sh
+++ tools/asdf/install.sh
@@ -1,4 +1,4 @@
 # https://asdf-vm.com/guide/getting-started.html
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 
-ln -sfnv ${HOME}/.asdf/.tool-versions ${HOME}/.config/asdf/.tool-versions
+ln -sfnv ${HOME}/.config/asdf/.tool-versions ${HOME}/.asdf/.tool-versions
