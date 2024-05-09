e60ba9de8e1c023318ca9e3f8e883016bad6b692
knsugiyama15 <knsugiyama15@gmail.com>
2024-05-09 18:16:35 +0900

    update

--- dist/Ubuntu/setup.sh
+++ dist/Ubuntu/setup.sh
@@ -4,7 +4,8 @@ set -eu
 
 echo y | sudo apt update
 echo y | sudo apt upgrade
-sudo apt install -y build-essential procps curl file git
+
+sudo apt install -y build-essential procps curl file git apt-transport-https ca-certificates software-properties-common
 
 # make ファイルのシンボリックリンクをrootに移動
 ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/Makefile ${HOME}/.dotfiles/Makefile
@@ -13,7 +14,15 @@ ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/.zshenv ${HOME}/.zshenv
 # Brewfile ファイル のシンボリックリンクをrootに移動
 ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/Brewfile ${HOME}/Brewfile
 
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ${HOME}/.profile
 source ~/.profile
+
