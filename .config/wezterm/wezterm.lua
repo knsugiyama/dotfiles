7977d5f792e743beec0f6bc292ca8270706abda7
knsugiyama15 <knsugiyama15@gmail.com>
2024-09-18 23:29:04 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -44,24 +44,6 @@ config.window_close_confirmation = "NeverPrompt"
 -- ショートカットキー設定
 local act = wezterm.action
 config.keys = {
-  -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
-  {
-    key = "f",
-    mods = "SHIFT|META",
-    action = act.ToggleFullScreen,
-  },
-  -- Ctrl+Shift+tで新しいタブを作成
-  {
-    key = "t",
-    mods = "SHIFT|CTRL",
-    action = act.SpawnTab("CurrentPaneDomain"),
-  },
-  -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
-  {
-    key = "d",
-    mods = "SHIFT|CTRL",
-    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
   -- Ctrl+Backspaceで前の単語を削除
   {
     key = "Backspace",
@@ -70,6 +52,82 @@ config.keys = {
       key = "w",
       mods = "CTRL",
     }),
+  },
+  -- Ctrl+Shift+tで新しいタブを作成
+  {
+    key = "t",
+    mods = "SHIFT|CTRL",
+    action = act.SpawnTab("CurrentPaneDomain"),
+  },
+  -- ⌘ Ctrl ,で下方向にペイン分割
+  {
+    key = ",",
+    mods = "CMD|CTRL",
+    action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+  },
+  -- ⌘ Ctrl .で右方向にペイン分割
+  {
+    key = ".",
+    mods = "CMD|CTRL",
+    action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+  },
+  -- ⌘ Ctrl oでペインの中身を入れ替える
+  {
+    key = "o",
+    mods = "CMD|CTRL",
+    action = wezterm.action.RotatePanes("Clockwise"),
+  },
+  -- ⌘ Ctrl hjklでペインの移動
+  {
+    key = "h",
+    mods = "CMD|CTRL",
+    action = wezterm.action.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "j",
+    mods = "CMD|CTRL",
+    action = wezterm.action.ActivatePaneDirection("Down"),
+  },
+  {
+    key = "k",
+    mods = "CMD|CTRL",
+    action = wezterm.action.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "l",
+    mods = "CMD|CTRL",
+    action = wezterm.action.ActivatePaneDirection("Right"),
+  },
+  -- ⌘ Ctrl Shift hjklでペイン境界の調整
+  {
+    key = "h",
+    mods = "CMD|CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize({ "Left", 2 }),
+  },
+  {
+    key = "j",
+    mods = "CMD|CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize({ "Down", 2 }),
+  },
+  {
+    key = "k",
+    mods = "CMD|CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize({ "Up", 2 }),
+  },
+  {
+    key = "l",
+    mods = "CMD|CTRL|SHIFT",
+    action = wezterm.action.AdjustPaneSize({ "Right", 2 }),
+  },
+}
+
+-- マウス操作の挙動設定
+config.mouse_bindings = {
+  -- 右クリックでクリップボードから貼り付け
+  {
+    event = { Down = { streak = 1, button = "Right" } },
+    mods = "NONE",
+    action = wezterm.action.PasteFrom("Clipboard"),
   },
 }
 
