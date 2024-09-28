97e4a5d74c22291feeb8ceb5ac044a004c1331b7
knsugiyama15 <knsugiyama15@gmail.com>
2024-09-28 10:11:48 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -59,64 +59,64 @@ config.keys = {
     mods = "SHIFT|CTRL",
     action = act.SpawnTab("CurrentPaneDomain"),
   },
-  -- ⌘ Ctrl ,で下方向にペイン分割
+  -- META Ctrl ,で下方向にペイン分割
   {
     key = ",",
-    mods = "CMD|CTRL",
+    mods = "META|CTRL",
     action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
   },
-  -- ⌘ Ctrl .で右方向にペイン分割
+  -- META Ctrl .で右方向にペイン分割
   {
     key = ".",
-    mods = "CMD|CTRL",
+    mods = "META|CTRL",
     action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
   },
-  -- ⌘ Ctrl oでペインの中身を入れ替える
+  -- META Ctrl oでペインの中身を入れ替える
   {
     key = "o",
-    mods = "CMD|CTRL",
+    mods = "META|CTRL",
     action = wezterm.action.RotatePanes("Clockwise"),
   },
-  -- ⌘ Ctrl hjklでペインの移動
+  -- META Ctrl hjklでペインの移動
   {
     key = "h",
-    mods = "CMD|CTRL",
+    mods = "META|CTRL",
     action = wezterm.action.ActivatePaneDirection("Left"),
   },
   {
     key = "j",
-    mods = "CMD|CTRL",
+    mods = "META|CTRL",
     action = wezterm.action.ActivatePaneDirection("Down"),
   },
   {
     key = "k",
-    mods = "CMD|CTRL",
+    mods = "META|CTRL",
     action = wezterm.action.ActivatePaneDirection("Up"),
   },
   {
     key = "l",
-    mods = "CMD|CTRL",
+    mods = "META|CTRL",
     action = wezterm.action.ActivatePaneDirection("Right"),
   },
-  -- ⌘ Ctrl Shift hjklでペイン境界の調整
+  -- META Ctrl Shift hjklでペイン境界の調整
   {
     key = "h",
-    mods = "CMD|CTRL|SHIFT",
+    mod = "META|CTRL|SHIFT",
     action = wezterm.action.AdjustPaneSize({ "Left", 2 }),
   },
   {
     key = "j",
-    mods = "CMD|CTRL|SHIFT",
+    mods = "META|CTRL|SHIFT",
     action = wezterm.action.AdjustPaneSize({ "Down", 2 }),
   },
   {
     key = "k",
-    mods = "CMD|CTRL|SHIFT",
+    mods = "META|CTRL|SHIFT",
     action = wezterm.action.AdjustPaneSize({ "Up", 2 }),
   },
   {
     key = "l",
-    mods = "CMD|CTRL|SHIFT",
+    mods = "META|CTRL|SHIFT",
     action = wezterm.action.AdjustPaneSize({ "Right", 2 }),
   },
 }
