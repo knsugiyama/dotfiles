536b1923a97f993ecabd7a82b727eef35908d617
knsugiyama15 <knsugiyama15@gmail.com>
2024-10-16 12:08:45 +0900

    update

--- dist/Ubuntu/setup.sh
+++ dist/Ubuntu/setup.sh
@@ -15,14 +15,13 @@ ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/.zshenv ${HOME}/.zshenv
 ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/Brewfile ${HOME}/Brewfile
 
 # docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-secho y | udo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-secho y | udo apt update
-secho y | udo apt install docker-ce docker-ce-cli containerd.io
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# secho y | udo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# 
+# secho y | udo apt update
+# secho y | udo apt install docker-ce docker-ce-cli containerd.io
 
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ${HOME}/.profile
 source ~/.profile
-
