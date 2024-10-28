d0e6769ac5c541c2114c1493af7ec3cc4da8a335
knsugiyama15 <knsugiyama15@gmail.com>
2024-10-28 23:51:06 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,7 +22,9 @@ config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
 
 -- change default shell
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  config.default_prog = { "pwsh.exe", "-l" }
+  -- config.default_prog = { "pwsh.exe", "-l" }
+  -- config.default_prog = { "wsl", "--exec", "/bin/bash", "-l" }
+  config.default_domain = "WSL:Ubuntu"
   config.font_size = 12.0
 end
 
@@ -41,10 +43,10 @@ config.leader = { key = "Space", mods = "CTRL|SHIFT" }
 config.window_background_opacity = 0.95
 config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 -- ショートカットキー設定
