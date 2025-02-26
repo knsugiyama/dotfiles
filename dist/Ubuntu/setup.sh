4c028cc7ac67e2c162fffd3eb8420453c25f5718
knsugiyama15 <knsugiyama15@gmail.com>
2025-02-26 11:36:00 +0900

    update

--- dist/Ubuntu/setup.sh
+++ dist/Ubuntu/setup.sh
@@ -14,16 +14,20 @@ ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/.zshenv ${HOME}/.zshenv
 # Brewfile ファイル のシンボリックリンクをrootに移動
 ln -sfnv ${HOME}/.dotfiles/dist/Ubuntu/Brewfile ${HOME}/Brewfile
 
-# docker
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# secho y | udo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# 
-# secho y | udo apt update
-# secho y | udo apt install docker-ce docker-ce-cli containerd.io
+# Docker のリポジトリを追加
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+# Docker および Docker-Compose のインストール
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # for WSL
 if [[ "$(uname -r)" == *microsoft* ]]; then
   echo -e "[boot]\nsystemd=true" | sudo tee /etc/wsl.conf
+  sudo systemctl enable --now docker
 fi
 
 # homebrew
